9dc55ea221dda3d2eabae1c183c6f04f1fc5573a
ngLuis <ngluisdev@gmail.com>
2022-04-01 19:09:07 +0200

    Add Matrix code on main.js file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # The Matrix Animation
+
+Code test by Ganesh Prasad on this [article](https://dev.to/gnsp/making-the-matrix-effect-in-javascript-din)
--- src/main.js
+++ src/main.js
@@ -1,1 +1,39 @@
-console.log("Hey");
+const canvas = document.getElementById('matrix-canvas');
+const canvasContext = canvas.getContext('2d');
+
+canvas.width = document.body.offsetWidth;
+canvas.height = document.body.offsetHeight;
+
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
+canvasContext.fillStyle = '#000000';
+canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
+
+const columnsNumber = Math.floor(canvasWidth / 20) + 1;
+const columns = Array(columnsNumber).fill(0);
+
+function matrix () {
+    canvasContext.fillStyle = '#0001';
+    canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
+  
+    canvasContext.fillStyle = '#0f0';
+    canvasContext.font = '15pt monospace';
+
+    columns.forEach((y, index) => {
+      const char = String.fromCharCode(Math.random() * 128);
+  
+      const x = index * 20;
+
+      canvasContext.fillText(char, x, y);
+  
+      if (y > 100 + Math.random() * 100000) {
+        columns[index] = 0;
+      } else {
+        columns[index] = y + 20
+      }
+    });
+}
+
+setInterval(matrix, 25);
+
